ddb48182e256c56de4193ad83b109c6525c627ae
EmmanuelAjibokun <emmanuelajibokun9@gmail.com>
2025-07-23 16:13:15 +0100

    Add: initiate upload page - set job form - set upload input

---
+++ app/lib/utils.ts
@@ -0,0 +1,18 @@
+/**
+ * Formats a file size in bytes to a human-readable string (KB, MB, GB)
+ * @param bytes -  The size in bytes
+ * @returns A formatted string with the appropriate unit
+ */
+
+export function formatFileSize(bytes: number) {
+    if (bytes === 0) return '0 Bytes';
+    if (isNaN(bytes) || !isFinite(bytes)) return 'Invalid';
+    const k = 1024;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    // Determine the appropriate unit by calculating the log
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    // Format with 2 decimal places and round
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+}
